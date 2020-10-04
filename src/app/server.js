f2b48384470976eee953b89bca3616a99d37127d
Atakan Gemici <atakan.gemici@ofisim.com>
2020-10-04 21:04:21 +0300

    bugfix.

--- src/app/server.js
+++ src/app/server.js
@@ -1,17 +1,14 @@
-//Install express server
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-express.static();
+app.use(express.static('./dist/friends'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/friends/src/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/friends' }
+  );
 });
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
